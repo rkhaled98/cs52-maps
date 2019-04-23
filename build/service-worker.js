bee4e47c7778d2bd5b726ce14524b86dc27b255b
Rafi Khaled <rkhaled@vassar.edu>
2019-04-23 15:15:22 -0400

    first commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.559e4cdaf062cd3091ef4cbdf572758c.js"
 );
 
 workbox.clientsClaim();
